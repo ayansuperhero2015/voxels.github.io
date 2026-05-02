ddf3bb5ffaa3ae0397fe71321ae0b890ad43acf2
ayansuperhero2015 <ayansuperhero2015@gmail.com>
2026-05-01 21:09:02 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,15 +4,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAWeouu2n0MEZAbzgk0jaEp0Nir6Dl4BFQ",
-  authDomain: "voxelcore-9dad7.firebaseapp.com",
-  projectId: "voxelcore-9dad7",
-  storageBucket: "voxelcore-9dad7.firebasestorage.app",
-  messagingSenderId: "551389120091",
-  appId: "1:551389120091:web:0fb60d3e2f2c0c5bc2217e",
-  databaseURL: "https://voxelcore-9dad7-default-rtdb.firebaseio.com"
-};
+(Import Api Key Here)
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
